29d1cb05ec7a5acaf0a22a643a2a58cd498dc85c
gosuenesy <82447771+gosuenesy@users.noreply.github.com>
2025-05-28 20:20:20 +0200

    skills fix

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,7 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import SkillCard from "./SkillCard.js";
+import { SkillCard } from "./SkillCard.js";
 
 import javascript from "../assets/skills/javascript.svg";
 import typescript from "../assets/skills/typescript.svg";
@@ -9,73 +9,59 @@ import reactIcon from "../assets/skills/react.svg";
 import vue from "../assets/skills/vue.svg";
 import csharp from "../assets/skills/csharp.svg";
 import dotnetcore from "../assets/skills/dotnetcore.svg";
-import angular from "../assets/skills/angular.svg";
 
 export default function Skills() {
-  const PrevArrow = ({ currentSlide, slideCount, ...props }) => {
-    const { onClick } = props;
+  const PrevArrow = ({ onClick }) => (
+    <div className="custom-prevArrow" onClick={onClick}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+      >
+        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+      </svg>
+    </div>
+  );
 
-    return (
-      <div {...props} className="custom-prevArrow" onClick={onClick}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-        >
-          <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
-        </svg>
-      </div>
-    );
-  };
-  const NextArrow = ({ currentSlide, slideCount, ...props }) => {
-    const { onClick } = props;
+  const NextArrow = ({ onClick }) => (
+    <div className="custom-nextArrow" onClick={onClick}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+      >
+        <path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z" />
+      </svg>
+    </div>
+  );
 
-    return (
-      <div {...props} className="custom-nextArrow" onClick={onClick}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-        >
-          <path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z" />
-        </svg>
-      </div>
-    );
-  };
   const settings = {
     dots: true,
     autoplay: true,
     infinite: true,
-    slidesToShow: 4,
+    centerMode: true,
+    centerPadding: "0px",
+    slidesToShow: 3,
     slidesToScroll: 1,
-    initialSlide: 0,
     speed: 500,
     arrows: true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
+          slidesToScroll: 1,
         },
       },
       {
-        breakpoint: 480,
+        breakpoint: 768,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
+          centerMode: true,
+          centerPadding: "0px",
         },
       },
     ],
@@ -86,42 +72,34 @@ export default function Skills() {
   return (
     <div
       id="skills"
-      className="mt-6 text-white"
-      style={{ justifyContent: "center" }}
+      className="scroll-mt-32 mt-10 text-white container mx-auto px-4"
     >
-      <h1
-        className="text-3xl font-bold text-center"
-        style={{ margin: "24px", marginTop: "40px" }}
-      >
-        Skills
-      </h1>
-      {/* <p className="font-light text-gray-400">Her er nogle af mine kompetencer</p> */}
-
-      <div className="mt-6">
-        <Slider {...settings}>
-          <SkillCard name="JavaScript" img={javascript} />
-          <SkillCard
-            name="TypeScript"
-            experience="Education and internship"
-            img={typescript}
-          />
-          <SkillCard
-            name="React"
-            experience="Education and internship"
-            img={reactIcon}
-          />
-          <SkillCard name="Vue.js" experience="Education" img={vue} />
-          {/* <SkillCard name="Angular" experience="Education" img={angular} /> */}
-          <SkillCard
-            name="React Native"
-            experience="Education"
-            img={reactIcon}
-          />
-          <SkillCard name="C#" experience="Education" img={csharp} />
-          <SkillCard name=".NET" experience="Education" img={dotnetcore} />
-        </Slider>
+      <h1 className="text-3xl font-bold text-center mb-10">Skills</h1>
+      <div className="flex justify-center">
+        <div className="w-full max-w-6xl">
+          <Slider {...settings}>
+            <SkillCard
+              name="JavaScript"
+              experience="Professional"
+              img={javascript}
+            />
+            <SkillCard
+              name="TypeScript"
+              experience="Professional"
+              img={typescript}
+            />
+            <SkillCard name="React" experience="Professional" img={reactIcon} />
+            <SkillCard name="Vue.js" experience="Education" img={vue} />
+            <SkillCard
+              name="React Native"
+              experience="Professional"
+              img={reactIcon}
+            />
+            <SkillCard name="C#" experience="Education" img={csharp} />
+            <SkillCard name=".NET" experience="Education" img={dotnetcore} />
+          </Slider>
+        </div>
       </div>
-      <br />
     </div>
   );
 }
