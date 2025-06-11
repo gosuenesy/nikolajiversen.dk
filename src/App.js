31c3198726d8de239ec79fd8e2f8724a093a31b8
gosuenesy <82447771+gosuenesy@users.noreply.github.com>
2025-06-11 04:21:27 +0200

    new background

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Coaching from "./components/Coaching";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import HrCurve from "./components/HrCurve";
+import bg from "./assets/bg.png";
 
 function App() {
   useEffect(() => {
@@ -19,8 +20,23 @@ function App() {
 
   return (
     <div className="relative overflow-hidden">
-      <div className="blob blob1"></div>
-      <div className="blob blob2"></div>
+      <div className="fixed inset-0 z-0">
+        {/* Image Layer */}
+        <div
+          className="w-full h-full bg-cover bg-no-repeat bg-center 
+               opacity-60 brightness-50"
+          style={{
+            backgroundImage: `url(${bg})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            transform: "scaleX(-1) translateX(10%)",
+            maskImage: "linear-gradient(to left, black 60%, transparent)",
+            WebkitMaskImage: "linear-gradient(to left, black 60%, transparent)",
+          }}
+        />
+      </div>
+      {/* <div className="blob blob1"></div> */}
+      {/* <div className="blob blob2"></div> */}
 
       <div className="relative z-10 px-6 lg:px-20 xl:px-36">
         <Navbar />
