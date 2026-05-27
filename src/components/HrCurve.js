3f23c29cc63046c757e40456809517f47f7b6d42
gosuenesy <82447771+gosuenesy@users.noreply.github.com>
2026-05-27 02:42:52 +0200

    hrcurve upgraded

--- src/components/HrCurve.js
+++ src/components/HrCurve.js
@@ -1,5 +1,5 @@
 export default function HrCurve() {
   return (
-    <div className="my-8 mx-auto w-1/5 h-[3px] rounded-full bg-gradient-to-r from-teal-800 to-teal-400" />
+    <div className="my-8 mx-auto w-1/5 h-[3px] rounded-full bg-gradient-to-r from-transparent via-teal-400 to-transparent" />
   );
 }
